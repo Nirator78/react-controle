f52a65f802ca6997b8d04ffb031498a14295f966
Nirator <clementduval786@gmail.com>
2022-01-15 18:40:05 +0100

    ajout case a cocher favori et refacto de la visualisation de la liste des pokemon

--- src/components/list/Item.js
+++ src/components/list/Item.js
@@ -4,11 +4,31 @@ import { Link } from "react-router-dom";
 export default function Item({data}) {
     const {name, image} = data;
 
+    const [checked, setChecked] = React.useState(false);
+
+    const handleChange = () => {
+        console.log("Changement d'etat", name);
+        setChecked(!checked);
+    };
+
     return (
-        <li className="flex">
-            {name}
-            <img src={image} alt="pokemon image" />
-            <Link to={`/pokemon/${name}`}>Voir {name}</Link>
-        </li>
+        <div className="border-2 border-gray-400">
+            <div className="flex items-center">
+                <img className="w-30 h-30 rounded-full mr-4" src={image} alt="Avatar of Jonathan Reinink"/>
+                <div className="text-sm">
+                    <p className="text-gray-900 leading-none">{name}</p>
+                    <p className="text-gray-600"><Link to={`/pokemon/${name}`}>Voir {name}</Link></p>
+                    <div className="flex items-center mr-4 mb-2">
+                        <input
+                            name={name}
+                            type="checkbox"
+                            checked={checked}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor={name} className="select-none"> Favori</label>
+                    </div>                   
+                </div>
+            </div>
+        </div>
     );
 }
--- src/components/pokemon-detail/type/Type.js
+++ src/components/pokemon-detail/type/Type.js
@@ -2,8 +2,6 @@ import React from 'react';
 import {colors} from '../../../constante/color';
 
 export default function Type({type}) {
-    
-
     return (
         <span className={"inline-block rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2"} style={{backgroundColor: colors[type]}}>
             {type}

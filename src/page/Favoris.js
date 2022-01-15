f3f58eb21fb1c0093ee6f29e948e96ad54125097
Nirator <clementduval786@gmail.com>
2022-01-15 20:33:41 +0100

    Affichage de la liste de pokemon favori

--- src/page/Favoris.js
+++ src/page/Favoris.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Cors from '../components/navbar/Cors';
+import { toJson } from '../utils/toJson';
 
 export default function Favoris (props) {
+
+    const [favoriList, setFavoriList] = useState([]);
+
+    useEffect(() =>{
+        // Récupération de la valeur favori dans le localstorage et on la converti en json traitable
+        let favori = toJson(localStorage.getItem('favori'));
+        
+        // Si oui on le re coche
+        if(favori.length){
+            setFavoriList(favori);
+        }
+    }, []);
+
     return (
         <div>
             <Cors>
-                <div className="font-bold">
-                    Favoris
+                <div>
+                    <h2 className="font-bold">Vos Pokémon favoris</h2>
+                    <br></br>
+                    <ul>
+                        {
+                            favoriList.map((data, key) => {
+                                return (
+                                    <li key={key}>
+                                        {data}
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
                 </div>
             </Cors>
         </div>

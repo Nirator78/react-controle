4b1aed7fa24d9b696282e97c90b7005a33e9ac39
Nirator <clementduval786@gmail.com>
2022-01-16 16:42:52 +0100

    Ajout logo pikachu animé

--- src/components/navbar/Header.js
+++ src/components/navbar/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import Lottie from 'react-lottie-player'
+
+import pokemonLottieJson from '../../lottie/pikachu'
+
 export default function Header () {
     return (
-        <h2 className='text-2xl font-normal leading-normal mt-0 mb-2 text-indigo-800'>
-            PokeClémentDex
-        </h2>
+        <div className="flex">
+            <Lottie
+                loop
+                animationData={pokemonLottieJson}
+                play
+                style={{ width: 40, height: 40 }}
+            />
+            <h2 className='text-2xl font-normal leading-normal mt-0 mb-2 text-indigo-800'>
+                PokeClémentDex
+            </h2>
+        </div>        
     )
 }

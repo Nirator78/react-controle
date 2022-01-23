294a41be5182deeb452d83964ed14d56ebfe071e
Nirator <clementduval786@gmail.com>
2022-01-23 17:20:30 +0100

    ajout de la police pokemon sur le nom de l'appli + rebuild

--- src/components/navbar/Header.js
+++ src/components/navbar/Header.js
@@ -6,7 +6,7 @@ export default function Header () {
     return (
         <div className="flex">
             <Pikachu width= {40} height= {40}/>
-            <h2 className='text-2xl font-normal leading-normal mt-0 mb-2 text-indigo-800'>
+            <h2 className='text-2xl font-normal leading-normal mt-0 mb-2 text-indigo-800 PokemonFont'>
                 PokeClémentDex
             </h2>
         </div>        
